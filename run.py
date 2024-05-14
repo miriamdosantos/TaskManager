5f6468f6062d359aa94608d07aea4055aa0c295e
Miriam McCay <mii.santos@yahoo.com.br>
2024-05-14 15:12:50 +0100

    Implementation function add_tasks

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def add_task(tasks):
+    name = input('Enter the task name: ')
+    due_date = input('Enter the due date (YYYY-MM-DD): ')
+    priority = input('Enter the priority (low, medium, high): ')
+    category = input('Enter the category: P - (Personal), B - (Business)')
+    description = input('Enter the description: ')
+    status = input('Enter the status (C - complete, P - pending, I - in progress): ')
+    assignee = input('Enter the assignee: ')
+    completion_date = input('Enter the completion date (YYYY-MM-DD): ')
+    
+    task = {
+        'name': name,
+        'due_date': due_date,
+        'priority': priority,
+        'category': category,
+        'description': description,
+        'status': status,
+        'assignee': assignee,
+        'completion_date': completion_date
+    }
+    
+    tasks.append(task)
+    print('Task successfully inserted')
+
+    return tasks
+
+def main():
+    tasks = []
+    print("Welcome to your TaskManager Data Automation") 
